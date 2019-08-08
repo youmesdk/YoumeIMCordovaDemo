52c495afd4dc327bdc8c26b6c433affbfd209c8c
fish <fish@youme.im>
2019-08-08 10:46:21 +0800

    prepare for iOS dev

--- README.md
+++ README.md
@@ -5,11 +5,26 @@ Youme IM Cordova Demo
 - Basic function for android platform is ready
 - iOS support is coming
 
-# Run demo
+# Run demo for android
 ```
 npm install -g cordova
 cordova platform add android
 cordova run android
+```
+
+# Run demo for iOS
+* need install xcode before running
+```
+sudo npm install -g cordova
+sudo npm install -g ios-deploy --unsafe-perm=true
+cordova platform add ios
+cordova run ios
+```
+
+# Update plugin
+```
+cordova plugin remove im.youme.cordovaim
+cordova plugin add /PathTo/YoumeIMCordovaPlugin
 ```
 
 # Core files for this demo
---
+++ platforms/ios/www/plugins/im.youme.cordovaim/www/YoumeIMCordovaPlugin.js
@@ -0,0 +1,66 @@
+cordova.define("im.youme.cordovaim.YoumeIMCordovaPlugin", function(require, exports, module) {
+var exec = require('cordova/exec');
+function YoumeIMCordovaPlugin(){
+    console.log('YoumeIMCordovaPlugin created')
+}
+
+YoumeIMCordovaPlugin.prototype.init = function (appKey,secretKey,regionId, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'init', [appKey,secretKey,regionId]);
+};
+YoumeIMCordovaPlugin.prototype.login = function (userid, password, token, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'login', [userid, password, token]);
+};
+YoumeIMCordovaPlugin.prototype.logout = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'logout', []);
+};
+YoumeIMCordovaPlugin.prototype.sendTextMessage = function (strRecvId, iChatType, strMsgContent, strAttachParam, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'sendTextMessage', [strRecvId, iChatType, strMsgContent, strAttachParam]);
+};
+
+YoumeIMCordovaPlugin.prototype.registerReconnectCallback = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'registerReconnectCallback', []);
+};
+YoumeIMCordovaPlugin.prototype.registerKickOffCallback = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'registerKickOffCallback', []);
+};
+YoumeIMCordovaPlugin.prototype.registerMsgEventCallback = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'registerMsgEventCallback', []);
+};
+YoumeIMCordovaPlugin.prototype.startRecordAudioMessage = function (recvID, chatType, extraText, needRecognize, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'startRecordAudioMessage', [recvID, chatType, extraText, needRecognize]);
+};
+YoumeIMCordovaPlugin.prototype.cancelAudioMessage = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'cancelAudioMessage', []);
+};
+YoumeIMCordovaPlugin.prototype.stopAndSendAudioMessage = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'stopAndSendAudioMessage', []);
+};
+
+YoumeIMCordovaPlugin.prototype.joinChatRoom = function (roomID, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'joinChatRoom', [roomID]);
+};
+
+YoumeIMCordovaPlugin.prototype.leaveChatRoom = function (roomID, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'leaveChatRoom', [roomID]);
+};
+
+YoumeIMCordovaPlugin.prototype.startPlayAudio = function (audioPath, success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'startPlayAudio', [audioPath]);
+};
+
+YoumeIMCordovaPlugin.prototype.stopPlayAudio = function (success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'stopPlayAudio', []);
+};
+
+/**
+ * transType: 0 message will delivery directly; 1 means just cc to app server, not delivery directly
+ */
+YoumeIMCordovaPlugin.prototype.switchTransType = function (transType,success, error) {
+    exec(success, error, 'YoumeIMCordovaPlugin', 'switchTransType', []);
+};
+
+var YoumeIMCordovaPluginInstance = new YoumeIMCordovaPlugin();
+module.exports = YoumeIMCordovaPluginInstance;
+
+
+});
